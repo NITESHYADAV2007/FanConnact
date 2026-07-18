3092e5a2da639f1ea5d91c94c8a6b74484ea83c9
khushbu-sys <khushbukumari.12.edu@gmail.com>
2026-07-19 04:05:03 +0530

    Initial commit: FanConnact Flutter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# fanconnact
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fanconnact/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fanconnact
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
